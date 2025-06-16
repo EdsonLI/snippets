949f1a62a4669dc61dcbc23588e58378efb38ed5
Edson Luís Isele <edson@dinabox.email>
2025-06-16 13:21:00 -0300

    Snippets

--- assets/scripts.js
+++ assets/scripts.js
@@ -216,49 +216,49 @@ $(document).ready(function() {
   }
 
   // Busca
-  $('#search').on('input', function() {
-    const searchText = $(this).val().toLowerCase();
+  // $('#search').on('input', function() {
+  //   const searchText = $(this).val().toLowerCase();
 
-    if (searchText.length > 1) {
-      $('.snippet-block').each(function() {
-        const tags = $(this).data('tags') || '';
-        const title = $(this).find('.snippet-title strong').text().toLowerCase();
-        const content = $(this).find('.snippet-content').text().toLowerCase();
+  //   if (searchText.length > 1) {
+  //     $('.snippet-block').each(function() {
+  //       const tags = $(this).data('tags') || '';
+  //       const title = $(this).find('.snippet-title strong').text().toLowerCase();
+  //       const content = $(this).find('.snippet-content').text().toLowerCase();
 
-        if (tags.includes(searchText) || title.includes(searchText) || content.includes(searchText)) {
-          $(this).show();
-          // Expandir o snippet para mostrar o resultado
-          $(this).find('.snippet-content').slideDown();
-          $(this).find('.snippet-title i:first-child:not(.fa-download)').removeClass('fa-expand').addClass('fa-compress');
-        } else {
-          $(this).hide();
-        }
-      });
+  //       if (tags.includes(searchText) || title.includes(searchText) || content.includes(searchText)) {
+  //         $(this).show();
+  //         // Expandir o snippet para mostrar o resultado
+  //         $(this).find('.snippet-content').slideDown();
+  //         $(this).find('.snippet-title i:first-child:not(.fa-download)').removeClass('fa-expand').addClass('fa-compress');
+  //       } else {
+  //         $(this).hide();
+  //       }
+  //     });
 
-      // Mostrar apenas categorias com snippets visíveis
-      $('.section[id!="madbuilder"][id!="vscode"]').each(function() {
-        if ($(this).find('.snippet-block:visible').length > 0) {
-          $(this).show();
-          // Mudar o ícone para baixo para categorias que ficam visíveis
-          $(this).find('h3 .collapse-icon').removeClass('fa-chevron-left').addClass('fa-chevron-down');
-          // Mostrar os controles da categoria
-          $(this).find('.category-controls').show();
-        } else {
-          $(this).hide();
-        }
-      });
-    } else {
-      // Restaurar a visibilidade padrão
-      $('.section, .snippet-block').show();
-      $('.snippet-content').hide();
-      $('.snippet-title i:first-child:not(.fa-download)').removeClass('fa-compress').addClass('fa-expand');
-      // Restaurar todos os ícones para baixo (expandido)
-      $('.collapse-icon').removeClass('fa-chevron-left').addClass('fa-chevron-down');
-      // Mostrar todos os controles de categoria
-      $('.category-controls').show();
-    }
-    addCopyButtons();
-  });
+  //     // Mostrar apenas categorias com snippets visíveis
+  //     $('.section[id!="madbuilder"][id!="vscode"]').each(function() {
+  //       if ($(this).find('.snippet-block:visible').length > 0) {
+  //         $(this).show();
+  //         // Mudar o ícone para baixo para categorias que ficam visíveis
+  //         $(this).find('h3 .collapse-icon').removeClass('fa-chevron-left').addClass('fa-chevron-down');
+  //         // Mostrar os controles da categoria
+  //         $(this).find('.category-controls').show();
+  //       } else {
+  //         $(this).hide();
+  //       }
+  //     });
+  //   } else {
+  //     // Restaurar a visibilidade padrão
+  //     $('.section, .snippet-block').show();
+  //     $('.snippet-content').hide();
+  //     $('.snippet-title i:first-child:not(.fa-download)').removeClass('fa-compress').addClass('fa-expand');
+  //     // Restaurar todos os ícones para baixo (expandido)
+  //     $('.collapse-icon').removeClass('fa-chevron-left').addClass('fa-chevron-down');
+  //     // Mostrar todos os controles de categoria
+  //     $('.category-controls').show();
+  //   }
+  //   addCopyButtons();
+  // });
 
   // Filtros rápidos (sugestões acima da busca)
   // $('.suggestions span').click(function() {
