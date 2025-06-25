b179670a18990d13103bc9a96443cd1a7ef9165b
EdsonLI <edson@dinabox.email>
2025-06-24 21:03:23 -0300

    Snippets

--- assets/js/main.js
+++ assets/js/main.js
@@ -212,4 +212,20 @@
   window.addEventListener('load', navmenuScrollspy);
   document.addEventListener('scroll', navmenuScrollspy);
 
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        e.preventDefault();
+        const offset = 20; // Ajuste o valor para descer mais
+        const targetPosition = targetElement.offsetTop - offset;
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+
 })();
