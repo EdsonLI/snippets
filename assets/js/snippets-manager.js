f6949b2c448a3ad5a2be2bdd41bd7f9e069d9994
EdsonLI <edson@dinabox.email>
2025-07-29 19:21:54 -0300

    TypeError: Cannot read properties of null (reading 'nodeName')

--- assets/js/snippets-manager.js
+++ assets/js/snippets-manager.js
@@ -470,12 +470,9 @@ document.addEventListener('DOMContentLoaded', function() {
       const sort = parent.getAttribute('data-sort') || 'original-order';
       
       // Aguardar carregamento de imagens antes de inicializar
-      imagesLoaded($container[0], function() {
-        // Verificar se o container existe antes de chamar imagesLoaded
-        if (!$container[0]) {
-          console.error('Erro: $container[0] é nulo ou indefinido.');
-          return;
-        }
+      if ($container[0]) {
+        imagesLoaded($container[0], function() {
+          // Criar nova instância do Isotope
         
         // Criar nova instância do Isotope
         const iso = new Isotope($container[0], {
