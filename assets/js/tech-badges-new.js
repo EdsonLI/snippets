7835293aed289248a1825bf94f13631262b9bf82
Edson Luís Isele <edson@dinabox.email>
2025-08-08 11:24:43 -0300

    Snippets

--- assets/js/tech-badges-new.js
+++ assets/js/tech-badges-new.js
@@ -112,7 +112,7 @@ document.addEventListener('DOMContentLoaded', function() {
           // Criar um elemento de imagem para o MadBuilder
           const madbuilderIcon = document.createElement('img');
           madbuilderIcon.className = 'madbuilder-icon';
-          madbuilderIcon.src = '/snippets/assets/img/madbuilder.jpg';
+          madbuilderIcon.src = '/snippets/assets/img/madbuilder.svg';
           madbuilderIcon.alt = 'MadBuilder';
           madbuilderIcon.width = 18;
           madbuilderIcon.height = 18;
