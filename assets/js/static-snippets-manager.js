7af22c41deef255b4a9880a05ff62b11ea8015d2
EdsonLI <edson@dinabox.email>
2025-08-17 19:28:21 -0300

    Snippets

--- assets/js/static-snippets-manager.js
+++ assets/js/static-snippets-manager.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
   };  const STATIC_DIRECTORY_MAP = {
     'ai': ['github-copilot.html', 'instructions_copilot_vscode.html'],
     'bootstrap': ['snippet_bootstrap_form_validation.html'],
-    'copilot': ['github-copilot.html'],
+    //'copilot': ['github-copilot.html'],
     'css': ['snippet_css_display_flex_center.html'],
     'git': ['snippet_git_change_all_message_commits.html', 'snippet_git_conflict_resolution_vscode.html', 'snippet_git_local_to_overwrite_remote.html', 'snippet_git_reset_hard_foce_push.html', 'snippet_git_stash.html', 'snippet_git_update_local_branch_from_development_and_other.html', 'snippet_git_update_local_branch_from_remote_development.html'],
     'isotope': ['snippet_isotope_basic_example.html'],
